3930be3e13fda8fb6634db54d211153b31abce2f
curryducker <66020230+curryducker@users.noreply.github.com>
2022-12-19 18:42:36 +0100

    bookshelf

--- src/main/java/net/larsmans/infinitybuttons/block/custom/secretbutton/BookshelfSecretButton.java
+++ src/main/java/net/larsmans/infinitybuttons/block/custom/secretbutton/BookshelfSecretButton.java
@@ -1,9 +1,12 @@
 package net.larsmans.infinitybuttons.block.custom.secretbutton;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.world.IWorldReader;
 
 public class BookshelfSecretButton extends AbstractSecretButton {
     public BookshelfSecretButton(Properties properties) {
@@ -24,5 +27,10 @@ public class BookshelfSecretButton extends AbstractSecretButton {
     protected SoundEvent getSoundEvent(boolean isOn) {
         // Credits to anne for suggesting this immensely satisfying sound
         return SoundEvents.BLOCK_BAMBOO_PLACE;
+    }
+
+    @Override
+    public float getEnchantPowerBonus(BlockState state, IWorldReader world, BlockPos pos) {
+        return 1;
     }
 }
