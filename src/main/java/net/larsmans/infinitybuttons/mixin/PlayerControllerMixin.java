921d414be9a68ee04f7c3675f0f720226987f50f
curryducker <jur.hoekman@gmail.com>
2023-06-17 15:47:40 +0200

    fix shield in hand

---
+++ src/main/java/net/larsmans/infinitybuttons/mixin/PlayerControllerMixin.java
@@ -0,0 +1,35 @@
+package net.larsmans.infinitybuttons.mixin;
+
+import net.larsmans.infinitybuttons.block.custom.emergencybutton.SafeEmergencyButton;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.client.entity.player.ClientPlayerEntity;
+import net.minecraft.client.multiplayer.PlayerController;
+import net.minecraft.client.network.play.ClientPlayNetHandler;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.network.play.client.CPlayerTryUseItemOnBlockPacket;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockRayTraceResult;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(PlayerController.class)
+public abstract class PlayerControllerMixin {
+
+    @Shadow @Final private ClientPlayNetHandler connection;
+
+    @Inject(method = "func_217292_a", at = @At("HEAD"), cancellable = true)
+    private void func_217292_a(ClientPlayerEntity player, ClientWorld world, Hand hand, BlockRayTraceResult p_217292_4_, CallbackInfoReturnable<ActionResultType> cir) {
+        BlockState state = world.getBlockState(p_217292_4_.getPos());
+        Block block = state.getBlock();
+        if (block instanceof SafeEmergencyButton) {
+            this.connection.sendPacket(new CPlayerTryUseItemOnBlockPacket(hand, p_217292_4_));
+            cir.setReturnValue(block.onBlockActivated(state, world, p_217292_4_.getPos(), player, hand, p_217292_4_));
+        }
+    }
+}
---
+++ src/main/java/net/larsmans/infinitybuttons/mixin/PlayerInteractionManagerMixin.java
@@ -0,0 +1,29 @@
+package net.larsmans.infinitybuttons.mixin;
+
+import net.larsmans.infinitybuttons.block.custom.emergencybutton.SafeEmergencyButton;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.management.PlayerInteractionManager;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(PlayerInteractionManager.class)
+public abstract class PlayerInteractionManagerMixin {
+
+    @Inject(method = "func_219441_a", at = @At("HEAD"), cancellable = true)
+    public void func_219441_a(ServerPlayerEntity playerIn, World worldIn, ItemStack stackIn, Hand handIn, BlockRayTraceResult blockRaytraceResultIn, CallbackInfoReturnable<ActionResultType> cir) {
+        BlockState state = worldIn.getBlockState(blockRaytraceResultIn.getPos());
+        Block block = state.getBlock();
+        if (block instanceof SafeEmergencyButton) {
+            cir.setReturnValue(block.onBlockActivated(state, worldIn, blockRaytraceResultIn.getPos(), playerIn, handIn, blockRaytraceResultIn));
+        }
+    }
+}
