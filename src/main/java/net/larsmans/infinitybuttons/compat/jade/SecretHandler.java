2d54fb5d15f4bd28b0e0f344aece4a4a55d0688f
curryducker <jur.hoekman@gmail.com>
2023-08-08 00:18:27 +0200

    type & jade registry names

--- src/main/java/net/larsmans/infinitybuttons/compat/jade/SecretHandler.java
+++ src/main/java/net/larsmans/infinitybuttons/compat/jade/SecretHandler.java
@@ -5,6 +5,7 @@ import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 import mcp.mobius.waila.Waila;
 import mcp.mobius.waila.addons.core.HUDHandlerBlocks;
+import mcp.mobius.waila.addons.core.PluginCore;
 import mcp.mobius.waila.api.IComponentProvider;
 import mcp.mobius.waila.api.IDataAccessor;
 import mcp.mobius.waila.api.IPluginConfig;
@@ -20,11 +21,13 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.registries.ForgeRegistries;
 import snownee.jade.JadePlugin;
 
 import java.util.List;
+import java.util.Objects;
 
 public class SecretHandler implements IComponentProvider {
 
@@ -65,79 +68,84 @@ public class SecretHandler implements IComponentProvider {
             try {
                 ITextComponent name = CACHE.get(accessor.getBlock(), () -> new TranslationTextComponent(((AbstractSecretButton) accessor.getBlock()).jadeBlock.getTranslationKey()));
                 ((ITaggableList<ResourceLocation, ITextComponent>)tooltip).setTag(HUDHandlerBlocks.OBJECT_NAME_TAG, new StringTextComponent(String.format(Waila.CONFIG.get().getFormatting().getBlockName(), name.getString())));
+                if (config.get(PluginCore.CONFIG_SHOW_REGISTRY)) {
+                    ((ITaggableList<ResourceLocation, ITextComponent>)tooltip).setTag(HUDHandlerBlocks.REGISTRY_NAME_TAG, (new StringTextComponent(Objects.requireNonNull(((AbstractSecretButton) accessor.getBlock()).jadeBlock.getRegistryName()).toString())).mergeStyle(TextFormatting.GRAY));
+                }
             } catch (Exception ignored) {
             }
-
         }
 
         if (hidden(InfinityButtonsPlugin.CONFIG_HIDE_SECRET_BUTTONS, config) && accessor.getBlock() instanceof HoglinMountButton) {
             try {
                 ITextComponent name = CACHE.get(accessor.getBlock(), () -> new TranslationTextComponent((HOGLIN_MOUNT.getTranslationKey())));
                 ((ITaggableList<ResourceLocation, ITextComponent>)tooltip).setTag(HUDHandlerBlocks.OBJECT_NAME_TAG, new StringTextComponent(String.format(Waila.CONFIG.get().getFormatting().getBlockName(), name.getString())));
+                if (config.get(PluginCore.CONFIG_SHOW_REGISTRY)) {
+                    ((ITaggableList<ResourceLocation, ITextComponent>)tooltip).setTag(HUDHandlerBlocks.REGISTRY_NAME_TAG, (new StringTextComponent(Objects.requireNonNull(HOGLIN_MOUNT.getRegistryName()).toString())).mergeStyle(TextFormatting.GRAY));
+                }
             } catch (Exception ignored) {
             }
-
         }
 
         if (hidden(InfinityButtonsPlugin.CONFIG_HIDE_TORCH_BUTTONS, config) && accessor.getBlock() instanceof TorchButton) {
             try {
                 ITextComponent name = CACHE.get(accessor.getBlock(), () -> new TranslationTextComponent(((TorchButton) accessor.getBlock()).jadeBlock.getTranslationKey()));
                 ((ITaggableList<ResourceLocation, ITextComponent>)tooltip).setTag(HUDHandlerBlocks.OBJECT_NAME_TAG, new StringTextComponent(String.format(Waila.CONFIG.get().getFormatting().getBlockName(), name.getString())));
+                if (config.get(PluginCore.CONFIG_SHOW_REGISTRY)) {
+                    ((ITaggableList<ResourceLocation, ITextComponent>)tooltip).setTag(HUDHandlerBlocks.REGISTRY_NAME_TAG, (new StringTextComponent(Objects.requireNonNull(((TorchButton) accessor.getBlock()).jadeBlock.getRegistryName()).toString())).mergeStyle(TextFormatting.GRAY));
+                }
             } catch (Exception ignored) {
             }
-
         }
 
         if (hidden(InfinityButtonsPlugin.CONFIG_HIDE_TORCH_BUTTONS, config) && accessor.getBlock() instanceof RedstoneTorchButton) {
             try {
                 ITextComponent name = CACHE.get(accessor.getBlock(), () -> new TranslationTextComponent(((RedstoneTorchButton) accessor.getBlock()).jadeBlock.getTranslationKey()));
                 ((ITaggableList<ResourceLocation, ITextComponent>)tooltip).setTag(HUDHandlerBlocks.OBJECT_NAME_TAG, new StringTextComponent(String.format(Waila.CONFIG.get().getFormatting().getBlockName(), name.getString())));
+                if (config.get(PluginCore.CONFIG_SHOW_REGISTRY)) {
+                    ((ITaggableList<ResourceLocation, ITextComponent>)tooltip).setTag(HUDHandlerBlocks.REGISTRY_NAME_TAG, (new StringTextComponent(Objects.requireNonNull(((RedstoneTorchButton) accessor.getBlock()).jadeBlock.getRegistryName()).toString())).mergeStyle(TextFormatting.GRAY));
+                }
             } catch (Exception ignored) {
             }
-
         }
     }
 
     @Override
     public void appendTail(List<ITextComponent> tooltip, IDataAccessor accessor, IPluginConfig config) {
-        if (!config.get(JadePlugin.HIDE_MOD_NAME)) {
-            if (hidden(InfinityButtonsPlugin.CONFIG_HIDE_SECRET_BUTTONS, config) && accessor.getBlock() instanceof AbstractSecretButton) {
-                String modName = ModIdentification.getModName(((AbstractSecretButton) accessor.getBlock()).jadeBlock);
-                if (!Strings.isNullOrEmpty(modName)) {
-                    modName = String.format(Waila.CONFIG.get().getFormatting().getModName(), modName);
-                    ((ITaggableList<ResourceLocation, ITextComponent>) tooltip).setTag(HUDHandlerBlocks.MOD_NAME_TAG, new StringTextComponent(modName));
-                }
+        if (!config.get(JadePlugin.HIDE_MOD_NAME) && hidden(InfinityButtonsPlugin.CONFIG_HIDE_SECRET_BUTTONS, config) && accessor.getBlock() instanceof AbstractSecretButton) {
+            String modName = ModIdentification.getModName(((AbstractSecretButton) accessor.getBlock()).jadeBlock);
+            if (!Strings.isNullOrEmpty(modName)) {
+                modName = String.format(Waila.CONFIG.get().getFormatting().getModName(), modName);
+                ((ITaggableList<ResourceLocation, ITextComponent>) tooltip).setTag(HUDHandlerBlocks.MOD_NAME_TAG, new StringTextComponent(modName));
             }
         }
 
-        if (!config.get(JadePlugin.HIDE_MOD_NAME)) {
-            if (hidden(InfinityButtonsPlugin.CONFIG_HIDE_SECRET_BUTTONS, config) && accessor.getBlock() instanceof HoglinMountButton) {
-                String modName = ModIdentification.getModName(HOGLIN_MOUNT);
-                if (!Strings.isNullOrEmpty(modName)) {
-                    modName = String.format(Waila.CONFIG.get().getFormatting().getModName(), modName);
-                    ((ITaggableList<ResourceLocation, ITextComponent>) tooltip).setTag(HUDHandlerBlocks.MOD_NAME_TAG, new StringTextComponent(modName));
-                }
+        if (!config.get(JadePlugin.HIDE_MOD_NAME) && hidden(InfinityButtonsPlugin.CONFIG_HIDE_SECRET_BUTTONS, config) && accessor.getBlock() instanceof HoglinMountButton) {
+            String modName = ModIdentification.getModName(HOGLIN_MOUNT);
+            if (!Strings.isNullOrEmpty(modName)) {
+                modName = String.format(Waila.CONFIG.get().getFormatting().getModName(), modName);
+                ((ITaggableList<ResourceLocation, ITextComponent>) tooltip).setTag(HUDHandlerBlocks.MOD_NAME_TAG, new StringTextComponent(modName));
             }
         }
 
-        if (!config.get(JadePlugin.HIDE_MOD_NAME)) {
-            if (hidden(InfinityButtonsPlugin.CONFIG_HIDE_TORCH_BUTTONS, config) && accessor.getBlock() instanceof TorchButton) {
-                String modName = ModIdentification.getModName(((TorchButton) accessor.getBlock()).jadeBlock);
-                if (!Strings.isNullOrEmpty(modName)) {
-                    modName = String.format(Waila.CONFIG.get().getFormatting().getModName(), modName);
-                    ((ITaggableList<ResourceLocation, ITextComponent>) tooltip).setTag(HUDHandlerBlocks.MOD_NAME_TAG, new StringTextComponent(modName));
-                }
+        if (!config.get(JadePlugin.HIDE_MOD_NAME) && hidden(InfinityButtonsPlugin.CONFIG_HIDE_TORCH_BUTTONS, config) && accessor.getBlock() instanceof TorchButton) {
+            String modName = ModIdentification.getModName(((TorchButton) accessor.getBlock()).jadeBlock);
+            if (!Strings.isNullOrEmpty(modName)) {
+                modName = String.format(Waila.CONFIG.get().getFormatting().getModName(), modName);
+                ((ITaggableList<ResourceLocation, ITextComponent>) tooltip).setTag(HUDHandlerBlocks.MOD_NAME_TAG, new StringTextComponent(modName));
             }
         }
 
-        if (!config.get(JadePlugin.HIDE_MOD_NAME)) {
-            if (hidden(InfinityButtonsPlugin.CONFIG_HIDE_TORCH_BUTTONS, config) && accessor.getBlock() instanceof RedstoneTorchButton) {
-                String modName = ModIdentification.getModName(((RedstoneTorchButton) accessor.getBlock()).jadeBlock);
-                if (!Strings.isNullOrEmpty(modName)) {
-                    modName = String.format(Waila.CONFIG.get().getFormatting().getModName(), modName);
-                    ((ITaggableList<ResourceLocation, ITextComponent>) tooltip).setTag(HUDHandlerBlocks.MOD_NAME_TAG, new StringTextComponent(modName));
-                }
+        if (!config.get(JadePlugin.HIDE_MOD_NAME) && hidden(InfinityButtonsPlugin.CONFIG_HIDE_TORCH_BUTTONS, config) && accessor.getBlock() instanceof RedstoneTorchButton) {
+            String modName = ModIdentification.getModName(((RedstoneTorchButton) accessor.getBlock()).jadeBlock);
+            if (!Strings.isNullOrEmpty(modName)) {
+                modName = String.format(Waila.CONFIG.get().getFormatting().getModName(), modName);
+                ((ITaggableList<ResourceLocation, ITextComponent>) tooltip).setTag(HUDHandlerBlocks.MOD_NAME_TAG, new StringTextComponent(modName));
             }
         }
+    }
+
+    @Override
+    public void appendBody(List<ITextComponent> tooltip, IDataAccessor accessor, IPluginConfig config) {
+        IComponentProvider.super.appendBody(tooltip, accessor, config);
     }
 }
